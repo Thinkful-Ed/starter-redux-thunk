525f2b17bedb125c5f52777d2ab48945530cdd9d
kchia <kchia87@gmail.com>
2022-06-15 15:11:16 -0400

    add complete code

--- src/features/quote/quote.slice.js
+++ src/features/quote/quote.slice.js
@@ -1,6 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit'; // Add createAsyncThunk to the list of imports
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-// Add fetchQuote() here
+export const fetchQuote = createAsyncThunk(
+  'quote/fetchQuote',
+  async (arg, thunkAPI) => {
+    const response = await fetch('/api/quote');
+    const { data } = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
   quote: '',
@@ -12,6 +19,21 @@ const options = {
   name: 'quote',
   initialState,
   reducers: {},
+  extraReducers: {
+    [fetchQuote.pending]: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [fetchQuote.fulfilled]: (state, { payload: { quote, author } }) => {
+      state.quote = { quote, author };
+      state.loading = false;
+      state.error = false;
+    },
+    [fetchQuote.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
 };
 
 const quoteSlice = createSlice(options);
